4714d3b0b9400076d3c122a6df494c21dcf63fc8
lperry34 <leviperry34@gmail.com>
2017-10-31 10:56:41 -0700

    set up routes folder and server folders

---
+++ routes/routes.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+  res.send('');
+})
+
+router.get('/', function (req, res) {
+  res.send('');
+})
+
+module.exports = router;
