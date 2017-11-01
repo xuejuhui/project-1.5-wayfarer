f3a08aa8730e8ab731e88efe5935ba211f8c23bf
sgc88 <sehribancheng@yahoo.com>
2017-11-01 14:40:19 -0700

    postmodal and images

--- src/PostModal.js
+++ src/PostModal.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import { Dropdown } from 'react-native-material-dropdown';
 
-<form>
- <div class="form-group">
-   <label for="inputEmail1">Email address</label>
-   <input type="email" class="form-control" id="inputEmail1" placeholder="Enter email">
- </div>
- <div class="form-group">
-   <label for="inputPassword1">Password</label>
-   <input type="password" class="form-control" id="inputPassword1" placeholder="Password">
- </div>
- <button type="submit" class="btn btn-default">Submit</button>
-</form>
+
+class Example extends Component {
+  render() {
+    let data = [{
+      value: 'London',
+    },
+    {
+      value: 'New York',
+    },
+    {
+      value: 'Vancouver',
+    },
+    {
+      value:'Taipei',
+    },
+    {
+      value:'Fujien'
+    },
+  ];
+
+    return (
+      <Dropdown
+        label='Favorite City'
+        data={data}
+      />
+    );
+
+  }
+}
+
 
 
 
