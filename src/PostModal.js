c217e66bd75bf77551e5a1f6d3c122f6858c8a13
sgc88 <sehribancheng@yahoo.com>
2017-11-01 11:51:52 -0700

    created new repo- first commit

---
+++ src/PostModal.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Dropdown } from 'react-native-material-dropdown';
+
+<form>
+ <div class="form-group">
+   <label for="inputEmail1">Email address</label>
+   <input type="email" class="form-control" id="inputEmail1" placeholder="Enter email">
+ </div>
+ <div class="form-group">
+   <label for="inputPassword1">Password</label>
+   <input type="password" class="form-control" id="inputPassword1" placeholder="Password">
+ </div>
+ <button type="submit" class="btn btn-default">Submit</button>
+</form>
+
+
+
+export default Postmodal;
