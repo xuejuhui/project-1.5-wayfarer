242ddf671d0517b6080b059b39b8162e005c2cda
Ray <xuejuhui@hotmail.com>
2017-11-03 11:49:57 -0700

    you can only delete your own post

--- src/Post.js
+++ src/Post.js
@@ -15,8 +15,10 @@ class Post extends Component {
   }
 
   render() {
+      if(this.props.currentUser===this.props.postUserId){
     let targetPost = this.props.postId
     return (
+
       <div onClick={e =>this.onClickExpand(e)}>
         <div className={`postComponent row ${this.state.expand}`}>
       <div className="col-xs-3">
@@ -31,7 +33,22 @@ class Post extends Component {
       </div>
       </div>
     );
-  }
+  }else {
+      return(
+            <div onClick={e =>this.onClickExpand(e)}>
+        <div className={`postComponent row ${this.state.expand}`}>
+      <div className="col-xs-3">
+      <img className="cityImg" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs4IcRyT_hLcN4VNMTj7M136R3FBH4tlT86UUQ7CZq5wnl4YgImg" alt="City"/>
+      </div>
+      <div className="col-xs-9">
+      <h4 style={{marginBottom:0}}>Post Title: {this.props.title}</h4>
+      <p className="description">{this.props.description}</p>
+  </div>
+      </div>
+      </div>
+
+      )}
+}
 }
 
 
